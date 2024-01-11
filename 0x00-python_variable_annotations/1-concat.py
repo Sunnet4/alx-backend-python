0c7d468793f23232420e8627f63d0ab4a54c0d3a
Sunnet4 <awolaja.sunday@gmail.com>
2024-01-11 13:44:54 -0800

    python

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module provides a function 'concat' that concatenates two strings.
+
+Author: Your Name
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two input strings.
+
+    :param str1: The first input string.
+    :param str2: The second input string.
+    :return: The concatenated string of 'str1' and 'str2'.
+    """
+    return str1 + str2
